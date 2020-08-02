b76a40a94d97f25aadc16f7359191d2c1113fb81
Norbert Herrmann <n.herrmann@physi.uni-heidelberg.de>
2020-08-02 10:27:41 +0200

    add timimg reference counter

--- MQ/unpacker/runUnpackTofMcbm2018.cxx
+++ MQ/unpacker/runUnpackTofMcbm2018.cxx
@@ -17,6 +17,9 @@ void addCustomOptions(bpo::options_description& options) {
   options.add_options()("ReqTint",
                         bpo::value<uint64_t>()->default_value(100),
                         "Time intervall length in ns");
+  options.add_options() ("ReqBeam",
+		  	  	  	  	bpo::value<uint64_t>()->default_value(-1),
+						"Mandatory beam counter in event");
   options.add_options()("PulserMode",
                         bpo::value<int64_t>()->default_value(0),
                         "Choose pulser configuration");
@@ -26,9 +29,24 @@ void addCustomOptions(bpo::options_description& options) {
   options.add_options()("PulTotMin",
                         bpo::value<uint64_t>()->default_value(0),
                         "Min pulser TimeOverThreshold");
-  options.add_options()("TShiftRef",
-                        bpo::value<double_t>()->default_value(0.),
-                        "Time shift of reference counter to match digis");
+   options.add_options() ("PulTotMax",
+			  	  	  	bpo::value<uint64_t>()->default_value(1000),
+						"Max pulser TimeOverThreshold");
+  options.add_options() ("ToffTof",
+			 	 	 	bpo::value<double_t>()->default_value(0.),
+						"Time offset of Tof digis with respect to T0");
+  options.add_options()("RefModType",
+                        bpo::value<int64_t>()->default_value(5),
+                        "Module type of reference counter");
+  options.add_options()("RefModId",
+                        bpo::value<int64_t>()->default_value(0),
+                        "Module Id of reference counter");
+  options.add_options()("RefCtrType",
+                        bpo::value<int64_t>()->default_value(4),
+                        "Counter type of reference counter");
+  options.add_options()("RefCtrId",
+                        bpo::value<int64_t>()->default_value(0),
+                        "Counter Id of reference counter");
   options.add_options()("MaxAsicInactive",
                         bpo::value<uint64_t>()->default_value(0),
                         "Max number of disabled ASICs");
