392c646f7daa20b59f1d8b1b4b88a7d736f7ce58
Norbert Herrmann <n.herrmann@physi.uni-heidelberg.de>
2020-08-04 08:37:28 +0200

    add geometry tools

---
+++ macro/mcbm/geometry/tof/create_parfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+VGEO=$1
+Setup=$2
+
+if [[ $Setup = "" ]]; then
+ Setup=mcbm_beam_2020_03
+fi 
+
+root -l -q Create_TOF_Geometry_${VGEO}.C
+#cp tof_v14a.root tof_${VGEO}.root
+cp tof_${VGEO}.geo.root ../../../../geometry/tof/
+
+#cp  tof_${VGEO}_geo.root geofile_tof_${VGEO}.root
+
+CURDIR=`pwd`
+cd ../..
+root -l -q './mcbm_transport.C(0,"'$Setup'")'
+cp -v ./test.geo.root     ./geometry/tof/geofile_tof_${VGEO}.root
+cp -v ./test.geo.root ../../geometry/tof/geofile_tof_${VGEO}.root
+cd $CURDIR
+
+root -l 'create_digipar.C("tof_'${VGEO}'")'
+cp tof_${VGEO}.digi.par      ../../../../parameters/tof/
+cp geofile_tof_${VGEO}.root  ../../../../geometry/tof/
