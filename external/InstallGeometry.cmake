734df31aa68e5efe8eec94add3710da562891f0c
Florian Uhlig <f.uhlig@gsi.de>
2020-08-28 11:01:37 +0200

    Update external geometry repository to the latest version
    Remove obsolete geometry from setup definition file.

--- external/InstallGeometry.cmake
+++ external/InstallGeometry.cmake
@@ -1,4 +1,4 @@
-set(GEOMETRY_VERSION def367210626d0b32620b13da9c8c205b6f8fd57)
+set(GEOMETRY_VERSION 02141012b58c0ffc8d1796ee002ef8b2bf488bde)
 
 set(GEOMETRY_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_geometry.git")
 
