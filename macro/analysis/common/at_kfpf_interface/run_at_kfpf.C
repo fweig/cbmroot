a332b5183a18bd93e65c786b9e65debe89a91e5c
Oleksii Lubynets <lubynets@lxbk0198.gsi.de>
2020-09-13 20:26:22 +0200

    add macro

---
+++ macro/analysis/common/at_kfpf_interface/run_at_kfpf.C
@@ -0,0 +1,18 @@
+void run_at_kfpf(const std::string& ATFile,
+                 int nEntries = -1,
+                 const std::string& ATTree = "aTree")
+{
+  ATKFParticleFinder man;
+  man.InitInput(ATFile.c_str(), ATTree.c_str());
+  man.InitOutput("KFPF.pwgc2f.root");
+  man.SetPIDMode(1);
+  
+  CutsContainer cuts;
+  cuts.SetCutChi2Prim(18.4207);
+  cuts.SetCutDistance(1.);
+  cuts.SetCutChi2Geo(3.);
+  cuts.SetCutLdL(5.);  
+  man.SetCuts(cuts);
+  
+  man.Run(nEntries);  
+}
