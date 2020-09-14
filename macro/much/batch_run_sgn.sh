0f891117364bec124c2914ef56159543e7c3ad57
Anna Senger <a.senger@gsi.de>
2020-09-14 10:27:05 +0200

    Update macro/much/batch_run_sgn.sh

--- macro/much/batch_run_sgn.sh
+++ macro/much/batch_run_sgn.sh
@@ -79,7 +79,7 @@ cp -rf ${work_dir_run}/run_reco_event.C .
 root -l -b -q "run_reco_event.C($NEVENTS,\"$DATASET\",\"$SETUP\",\"$USEMC\",kTRUE)"
 
 cp -rf ${work_dir_much}/run_ana.C .
-root -l -b -q "run_ana.C($NEVENTS,\"$DATASET\",\"$SETUP\",\"$USEMC\",\"$PLUTOFILE\")"
+root -l -b -q "run_ana.C($NEVENTS,\"$DATASET\",\"$SETUP\",\"$USEMC\",\"$PLUTOFILE\",-1)"
 
 rm all_*
 rm *C
