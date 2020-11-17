b95ae2e97737fcadaf9b15ba99e27110ef54a6f5
slebedev <s.lebedev@gsi.de>
2020-11-17 22:07:04 +0100

    Update format for macros

--- macro/littrack/radlength_sim.C
+++ macro/littrack/radlength_sim.C
@@ -4,10 +4,12 @@ using std::cout;
 using std::endl;
 
 void radlength_sim(
-  const string& mcFile = "/Users/slebedev/Development/cbm/data/sim/rich/radlen/mc.ac.root",
-  const string& parFile = "/Users/slebedev/Development/cbm/data/sim/rich/radlen/param.ac.root",
-  const string &geoSetup = "sis100_electron_rich_pal_bcarb", 
-  Int_t nofEvents = 0) {
+  const string& mcFile =
+    "/Users/slebedev/Development/cbm/data/sim/rich/radlen/mc.ac.root",
+  const string& parFile =
+    "/Users/slebedev/Development/cbm/data/sim/rich/radlen/param.ac.root",
+  const string& geoSetup = "sis100_electron_rich_pal_bcarb",
+  Int_t nofEvents        = 0) {
 
   TTree::SetMaxTreeSize(90000000000);
 
@@ -17,23 +19,23 @@ void radlength_sim(
   TStopwatch timer;
   timer.Start();
 
-//   const Double_t minX   = -550;  // cm
-//   const Double_t maxX   = 550;   // cm
-//   const Double_t stepX  = 10.;    // cm
-//   const Int_t nofBinsX  = (maxX - minX) / stepX;
-//   const Double_t minY   = -550;  // cm
-//   const Double_t maxY   = 550;   // cm
-//   const Double_t stepY  = 10.;    // cm
+  //   const Double_t minX   = -550;  // cm
+  //   const Double_t maxX   = 550;   // cm
+  //   const Double_t stepX  = 10.;    // cm
+  //   const Int_t nofBinsX  = (maxX - minX) / stepX;
+  //   const Double_t minY   = -550;  // cm
+  //   const Double_t maxY   = 550;   // cm
+  //   const Double_t stepY  = 10.;    // cm
 
-  const Double_t minX   = -250;  // cm
-  const Double_t maxX   = 250;   // cm
-  const Double_t stepX  = 1.;    // cm
-  const Int_t nofBinsX  = (maxX - minX) / stepX;
-  const Double_t minY   = -250;  // cm
-  const Double_t maxY   = 250;   // cm
-  const Double_t stepY  = 1.;    // cm
-  const Int_t nofBinsY  = (maxY - minY) / stepY;
-  nofEvents = nofBinsX * nofBinsY;
+  const Double_t minX  = -250;  // cm
+  const Double_t maxX  = 250;   // cm
+  const Double_t stepX = 1.;    // cm
+  const Int_t nofBinsX = (maxX - minX) / stepX;
+  const Double_t minY  = -250;  // cm
+  const Double_t maxY  = 250;   // cm
+  const Double_t stepY = 1.;    // cm
+  const Int_t nofBinsY = (maxY - minY) / stepY;
+  nofEvents            = nofBinsX * nofBinsY;
 
   std::vector<Double_t> vectorX, vectorY;
   for (Int_t iX = 0; iX < nofBinsX; iX++) {
@@ -69,21 +71,22 @@ void radlength_sim(
    primGen->AddGenerator(box);
 */
 
-    run.SetOutFileName(mcFile.c_str());
-    run.SetParFileName(parFile.c_str());
-    run.LoadSetup(geoSetup.c_str());
-    run.SetTarget("Gold", 0.025, 2.5);
-    run.SetBeamPosition(0., 0., 0., 0.);
-    //run.SetEngine(kGeant4);
-    //run.StoreTrajectories(true);
-    run.RegisterRadLength(true);
-    run.Run(nofEvents);
+  run.SetOutFileName(mcFile.c_str());
+  run.SetParFileName(parFile.c_str());
+  run.LoadSetup(geoSetup.c_str());
+  run.SetTarget("Gold", 0.025, 2.5);
+  run.SetBeamPosition(0., 0., 0., 0.);
+  //run.SetEngine(kGeant4);
+  //run.StoreTrajectories(true);
+  run.RegisterRadLength(true);
+  run.Run(nofEvents);
 
-    timer.Stop();
-    std::cout << std::endl << std::endl;
-    std::cout << "Macro finished successfully." << std::endl;
-    std::cout << "MC file is "    << mcFile << std::endl;
-    std::cout << "Parameter file is " << parFile << std::endl;
-    std::cout << "Real time " << timer.RealTime() << " s, CPU time " << timer.CpuTime() << "s" << std::endl;
-    std::cout << std::endl << "Test passed" << std::endl << "All ok" << std::endl;
+  timer.Stop();
+  std::cout << std::endl << std::endl;
+  std::cout << "Macro finished successfully." << std::endl;
+  std::cout << "MC file is " << mcFile << std::endl;
+  std::cout << "Parameter file is " << parFile << std::endl;
+  std::cout << "Real time " << timer.RealTime() << " s, CPU time "
+            << timer.CpuTime() << "s" << std::endl;
+  std::cout << std::endl << "Test passed" << std::endl << "All ok" << std::endl;
 }
