86474e2fe840d2c06011748b892649ee663a3348
Florian Uhlig <f.uhlig@gsi.de>
2020-12-16 16:33:51 +0100

    Use latest version of parameter repository

--- external/InstallParameter.cmake
+++ external/InstallParameter.cmake
@@ -1,4 +1,4 @@
-set(PARAMETER_VERSION e075e9e36f68c41b181bb2ef5003a0aaa041f11a)
+set(PARAMETER_VERSION 2c5fffe5eef95db9b40f11e1d569f4740ba40805)
 
 set(PARAMETER_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_parameter.git")
 
