fe80cf7ea71d9345dd8d395fd4185ee005c19bf9
Eoin Clerkin <e.clerkin@ygsi.de>
2021-02-26 23:34:31 +0100

    Updates to use latest geometry tags.
    Uses recommended subsystem versions in the default setup files.

--- external/InstallGeometry.cmake
+++ external/InstallGeometry.cmake
@@ -1,4 +1,4 @@
-set(GEOMETRY_VERSION af462dd0c6a1c8813c498d174e5dcc6bcda516c7)
+set(GEOMETRY_VERSION 3bb7564c1f3983233a72370527a32a8be1c86ffd)
 
 set(GEOMETRY_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_geometry.git")
 
