1e94776d69163f8d1f22a4ca3a25ecd10eb754b2
Eoin Clerkin <e.clerkin@gsi.de>
2021-03-18 18:02:21 +0100

    Seting repo to official geometry repo again.

--- external/InstallGeometry.cmake
+++ external/InstallGeometry.cmake
@@ -1,6 +1,6 @@
 set(GEOMETRY_VERSION 151fcb9356610da62d91eeb7d5f4ef3d418b0cae)
 
-set(GEOMETRY_SRC_URL "https://git.cbm.gsi.de/e.clerkin/cbmroot_geometry.git")
+set(GEOMETRY_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_geometry.git")
 
 download_project_if_needed(PROJECT         Geometry_source
                            GIT_REPOSITORY  ${GEOMETRY_SRC_URL}
