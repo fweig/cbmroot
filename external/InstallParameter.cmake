43df683242b07b47a49e0ab75505f4af3cd49ea3
Eoin Clerkin <e.clerkin@gsi.de>
2021-03-18 17:37:01 +0100

    Reverts the parameter back to official copy.
    Reverts the geometry repo back to official with updated tag.

--- external/InstallParameter.cmake
+++ external/InstallParameter.cmake
@@ -1,6 +1,6 @@
-set(PARAMETER_VERSION 4aea00fc0ff47ae7a79e9b60bcd6d3ddd1f9a766)
+set(PARAMETER_VERSION 68f0b16c6edace4faaa09f460682173772081976)
 
-set(PARAMETER_SRC_URL "https://git.cbm.gsi.de/e.clerkin/cbmroot_parameter.git")
+set(PARAMETER_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_parameter.git")
 
 download_project_if_needed(PROJECT         Parameter_source
                            GIT_REPOSITORY  ${PARAMETER_SRC_URL}
