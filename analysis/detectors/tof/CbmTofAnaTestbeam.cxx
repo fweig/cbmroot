48cc7cb63b827fd81b02f08fc49898602b2c3b08
Norbert Herrmann <n.herrmann@physi.uni-heidelberg.de>
2021-04-08 11:31:12 +0200

    format

--- analysis/detectors/tof/CbmTofAnaTestbeam.cxx
+++ analysis/detectors/tof/CbmTofAnaTestbeam.cxx
@@ -5246,8 +5246,7 @@ Bool_t CbmTofAnaTestbeam::FillHistos() {
           }
         }
         if (iM0 == iNbMatchedHits) {
-          LOG(debug) << Form("no valid match in %d hits for Addr 0x%08x found ", iNbMatchedHits,
-                             pHitRef->GetAddress());
+          LOG(debug) << Form("no valid match in %d hits for Addr 0x%08x found ", iNbMatchedHits, pHitRef->GetAddress());
           if (fair::Logger::Logging(fair::Severity::debug1)) LOG(fatal) << "Check for consistency!";
           return 0;
         }
