10bf5f244eb3f1029a012b982994168c5122c927
Valentina <v.akishina@gsi.de>
2021-09-14 20:50:55 +0200

    edit licence

--- reco/L1/CbmL1ReadEvent.cxx
+++ reco/L1/CbmL1ReadEvent.cxx
@@ -1,6 +1,6 @@
 /* Copyright (C) 2006-2021 GSI Helmholtzzentrum fuer Schwerionenforschung, Darmstadt
    SPDX-License-Identifier: GPL-3.0-only
-   Authors: Ivan Kisel,  Sergey Gorbunov, Irina Rostovtseva, Valentina Akishina, Maksym Zyzak */
+   Authors: Ivan Kisel,  Sergey Gorbunov [committer], Irina Rostovtseva, Valentina Akishina, Maksym Zyzak */
 
 /*
  *====================================================================
