d72a3756dad33e65abc9aa4e8a0ae07c791d7049
Alexandru Bercuci <abercuci@niham.nipne.ro>
2021-11-04 13:33:19 +0200

    fix dimensions for the 2D chambers

--- macro/mcbm/geometry/trd/Create_TRD_Geometry_v21b.C
+++ macro/mcbm/geometry/trd/Create_TRD_Geometry_v21b.C
@@ -414,8 +414,8 @@ Double_t asic_distance;
 //const Double_t FrameWidth[2]    = { 1.5, 2.0 };   // Width of detector frames in cm
 const Double_t FrameWidth[4] = {1.5, 1.5, 2.5, 1.5};  // Width of detector frames in cm
 // mini - production
-const Double_t DetectorSizeX[4] = {57., 95., 59.0, 23 + 3.};   // => 54 x 54 cm2 & 91 x 91 cm2 active area
-const Double_t DetectorSizeY[4] = {57., 95., 60.8, 8.16 + 3};  // quadratic modules
+const Double_t DetectorSizeX[4] = {57., 95., 59.0, 23.04 + 3.};   // => 54 x 54 cm2 & 91 x 91 cm2 active area
+const Double_t DetectorSizeY[4] = {57., 95., 60.8, 8.16 + 3.};  // quadratic modules
 //// default
 //const Double_t DetectorSizeX[2] = { 60., 100.};   // => 57 x 57 cm2 & 96 x 96 cm2 active area
 //const Double_t DetectorSizeY[2] = { 60., 100.};   // quadratic modules
@@ -820,7 +820,7 @@ void dump_digi_file()
       else
         fprintf(ifile, "      ");
 
-      fprintf(ifile, "{ %.1f, %5.2f, %.1f/%3d, %5.2f }", ActiveAreaX[ModuleType[im]], HeightOfSector[im][is],
+      fprintf(ifile, "{ %.2f, %5.2f, %.2f/%3d, %5.2f }", ActiveAreaX[ModuleType[im]], HeightOfSector[im][is],
               ActiveAreaX[ModuleType[im]], NofPadsInRow[ModuleType[im]], PadHeightInSector[im][is]);
 
       if ((im == NofModuleTypes - 1) && (is == 2)) fprintf(ifile, " } };");
