8f1f205b5f28433315c4e615c1fe9fd549c495ea
Shreya Roy <s.roy@gsi.de>
2021-11-18 16:04:18 +0100

    tof v22a geo macro

--- macro/mcbm/geometry/tof/Create_TOF_Geometry_v22a_mcbm.C
+++ macro/mcbm/geometry/tof/Create_TOF_Geometry_v22a_mcbm.C
@@ -1,4 +1,4 @@
-/** @file Create_TOF_Geometry_v20d_mcbm.C
+/** @file Create_TOF_Geometry_v22a_mcbm.C
   * @copyright Copyright (C) 2020 GSI Helmholtzzentrum fuer Schwerionenforschung, Darmstadt
   * @license SPDX-License-Identifier: GPL-3.0-only
   * @authors Florian Uhlig [orginator] **/
