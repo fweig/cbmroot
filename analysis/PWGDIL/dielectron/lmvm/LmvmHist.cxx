ebc41d6d09f9d182afc7c829f2e1e54f5c211e82
Cornelius Feier-Riesen <cornelius.riesen@physik.uni-giessen.de>
2021-12-08 09:19:07 +0100

    Correct start year and comitter

--- analysis/PWGDIL/dielectron/lmvm/LmvmHist.cxx
+++ analysis/PWGDIL/dielectron/lmvm/LmvmHist.cxx
@@ -1,6 +1,6 @@
-/* Copyright (C) 2021 Justus-Liebig-Universitaet Giessen, Giessen
+/* Copyright (C) 2012-2021 UGiessen, JINR-LIT
    SPDX-License-Identifier: GPL-3.0-only
-   Authors: Semen Lebedev [committer] */
+   Authors: Elena Lebedeva, Semen Lebedev [committer] */
 
 #include "LmvmHist.h"
 
