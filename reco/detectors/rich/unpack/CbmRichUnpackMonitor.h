155834715b5f9899a22fe220801b3910b130c479
Adrian Weber <adrian.a.weber@physik.uni-giessen.de>
2022-02-14 14:44:22 +0100

    correct license information

--- reco/detectors/rich/unpack/CbmRichUnpackMonitor.h
+++ reco/detectors/rich/unpack/CbmRichUnpackMonitor.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2022 Justus-Liebig-University Gießen, Gießen
+/* Copyright (C) 2022 Justus-Liebig-Universität Gießen, Gießen
    SPDX-License-Identifier: GPL-3.0-only
    Authors: Adrian Weber [committer]*/
 
