f7ee6b9e8209738ff17381ebf1a44f97b0d6f55f
s.zharko@gsi.de <s.zharko@gsi.de>
2022-02-24 22:01:56 +0100

    license bug fixed

--- reco/L1/L1Algo/L1UMeasurementInfo.cxx
+++ reco/L1/L1Algo/L1UMeasurementInfo.cxx
@@ -1,6 +1,6 @@
 /* Copyright (C) 2007-2022 GSI Helmholtzzentrum fuer Schwerionenforschung, Darmstadt
    SPDX-License-Identifier: GPL-3.0-only
-   Authors: Sergey Gorbunov, Sergei Zharko */
+   Authors: Sergey Gorbunov, Sergei Zharko [committer] */
 
 #include "L1UMeasurementInfo.h"
 
