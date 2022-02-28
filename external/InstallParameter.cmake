37b8983ed6700d48147a7bff4022e3472d923ee7
Valentina <v.akishina@gsi.de>
2022-02-28 19:33:22 +0100

    Update parameter hash

--- external/InstallParameter.cmake
+++ external/InstallParameter.cmake
@@ -1,4 +1,4 @@
-set(PARAMETER_VERSION 849f5cf315a9c8ac40dc9e93fa90015b1535f6e4)
+set(PARAMETER_VERSION 2e81209afbc147842d68e563bc08bc91256fae1b)
 
 set(PARAMETER_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_parameter.git")
 
