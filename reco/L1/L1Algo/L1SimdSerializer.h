3f37ad42c682275f27da32e30e5c5a388258fab4
s.zharko@gsi.de <s.zharko@gsi.de>
2022-09-07 16:02:44 +0200

    L1 [bugfix]: reimplemented serialization of SIMD vectors

--- reco/L1/L1Algo/L1SimdSerializer.h
+++ reco/L1/L1Algo/L1SimdSerializer.h
@@ -11,19 +11,36 @@
 #define L1SimdSerializer_h 1
 
 #include <boost/serialization/access.hpp>
+#include <boost/serialization/split_free.hpp>
 
 #include "vectors/L1vec.h"
-
+/// This header defines functionality for saving and loading SIMDized vectors. At the moment, only the first element of
+/// the vector can be saved, and the loaded vector will be horizontally equal.
+///
 namespace boost
 {
   namespace serialization
   {
     template<class Archive>
-    void serialize(Archive& ar, fvec& vect, const unsigned int)
+    void save(Archive& ar, const fvec& vect, unsigned int)
     {
+      ar << vect[0];
+    }
+
+    template<class Archive>
+    void load(Archive& ar, fvec& vect, unsigned int)
+    {
+      fscal buffer;
+      ar >> buffer;
       for (size_t i = 0; i < fvec::size(); ++i) {
-        ar&(reinterpret_cast<fscal*>(&vect))[i];
+        vect[i] = buffer;
       }
+    }
+
+    template<class Archive>
+    void serialize(Archive& ar, fvec& vect, const unsigned int version)
+    {
+      split_free(ar, vect, version);
     }
   }  // namespace serialization
 }  // namespace boost
