47e5dddfea5cc73915f0e6a1312ea8a70cda70cf
alberica toia <a.toia@gsi.de>
2022-10-05 14:35:08 +0200

    update the parameters hash

--- external/InstallParameter.cmake
+++ external/InstallParameter.cmake
@@ -1,4 +1,4 @@
-set(PARAMETER_VERSION 92ccc2608756fc38186cd74af3667a1443bd0b77)
+set(PARAMETER_VERSION 9835279a7166989062eab6654af7e9c256028106)
 
 set(PARAMETER_SRC_URL "https://git.cbm.gsi.de/CbmSoft/cbmroot_parameter.git")
 
